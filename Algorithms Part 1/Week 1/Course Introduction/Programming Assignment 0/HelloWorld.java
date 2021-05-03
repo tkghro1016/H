84121cb0f3f645dc103138b0e47858ad9d310692
tkghro1016 <tkghro1016@gmail.com>
2021-05-03 21:56:23 +0900

    --

---
+++ Algorithms Part 1/Week 1/Course Introduction/Programming Assignment 0/HelloWorld.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Lee Ki Heun
+ *  Coursera User ID:  tkghro1016@gmail.com
+ *  Last modified:     May 16, 2021
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
