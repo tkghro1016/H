d95314d662c332a8ac3450a3df03b564e68b5844
tkghro1016 <tkghro1016@gmail.com>
2021-05-23 21:24:14 +0900

    3rd Assign. ~ing

---
+++ Algorithms Part 1/hello/FastCollinearPoints.java
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ *  Name:              Lee Ki Heun
+ *  Coursera User ID:  tkghro1016@gmail.com
+ *  Last modified:     May 23, 2021
+ **************************************************************************** */
+
+public class FastCollinearPoints {
+
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+    }
+
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return null;
+    }
+}
