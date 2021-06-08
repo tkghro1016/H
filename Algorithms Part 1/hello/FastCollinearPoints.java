0e96942e298d3e948b49ce0a2b70800f65c6bb0f
tkghro1016 <tkghro1016@gmail.com>
2021-06-08 21:45:38 +0900

    --

--- Algorithms Part 1/hello/FastCollinearPoints.java
+++ Algorithms Part 1/hello/FastCollinearPoints.java
@@ -4,9 +4,11 @@
  *  Last modified:     May 23, 2021
  **************************************************************************** */
 
+
+import edu.princeton.cs.algs4.Insertion;
+
 public class FastCollinearPoints {
     private LineSegment[] lineList;
-    private Point[] pointList;
     private int index;
 
     // finds all line segments containing 4 or more points
@@ -17,7 +19,7 @@ public class FastCollinearPoints {
 
 
         // input을 바로 가리키지 않게 복사
-        pointList = new Point[points.length];
+        Point[] pointList = new Point[points.length];
         for (int i = 0; i < points.length; i++) {
             if (points[i] == null) {
                 throw new IllegalArgumentException();
@@ -27,8 +29,7 @@ public class FastCollinearPoints {
         index = 0;
         lineList = new LineSegment[pointList.length];
         sortByCoord(pointList);
-        mainFunc();
-        lineList = segments();
+        mainFunc(pointList);
     }
 
     // the number of line segments
@@ -45,39 +46,34 @@ public class FastCollinearPoints {
         return copy;
     }
 
-    private void mergeByCoord(Point[] points, Point[] aux, int lo, int mid, int hi) {
+    private void mergeByCoord(Point[] pointList, Point[] aux, int lo, int mid, int hi) {
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; k++) {
-            if (i > mid) aux[k] = points[j++];
-            else if (j > hi) aux[k] = points[i++];
-            else if (points[i].compareTo(points[j]) > 0) aux[k] = points[j++];
-            else aux[k] = points[i++];
+            if (i > mid) aux[k] = pointList[j++];
+            else if (j > hi) aux[k] = pointList[i++];
+            else if (pointList[i].compareTo(pointList[j]) > 0) aux[k] = pointList[j++];
+            else aux[k] = pointList[i++];
         }
     }
 
-    private void sortByCoord(Point[] points, Point[] aux, int lo, int hi) {
-        if (hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-        sortByCoord(aux, points, lo, mid);
-        sortByCoord(aux, points, mid + 1, hi);
-        if (points[mid + 1].compareTo(points[mid]) > 0) {
-            for (int i = lo; i <= hi; i++) aux[i] = points[i];
+    private void sortByCoord(Point[] pointList, Point[] aux, int lo, int hi) {
+        if (hi <= lo + 7) {
+            Insertion.sort(aux, lo, hi);
             return;
         }
-        mergeByCoord(points, aux, lo, mid, hi);
+        int mid = lo + (hi - lo) / 2;
+        sortByCoord(aux, pointList, lo, mid);
+        sortByCoord(aux, pointList, mid + 1, hi);
+        mergeByCoord(pointList, aux, lo, mid, hi);
     }
 
-    private void sortByCoord(Point[] points) {
-        Point[] aux = new Point[pointList.length];
-        for (int i = 0; i < pointList.length; i++) {
-            aux[i] = pointList[i];
-        }
-        sortByCoord(aux, points, 0, pointList.length - 1);
+    private void sortByCoord(Point[] pointList) {
+        Point[] aux = pointList.clone();
+        sortByCoord(aux, pointList, 0, pointList.length - 1);
     }
 
     private void mergeBySlope(Point source, Point[] orderList, Point[] aux, int lo, int mid,
                               int hi) {
-
         int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; k++) {
             if (i > mid) aux[k] = orderList[j++];
@@ -93,20 +89,14 @@ public class FastCollinearPoints {
         int mid = lo + (hi - lo) / 2;
         sortBySlope(source, aux, orderList, lo, mid);
         sortBySlope(source, aux, orderList, mid + 1, hi);
-        if (source.slopeOrder().compare(orderList[mid + 1], orderList[mid]) > 0) {
-            for (int i = lo; i <= hi; i++) aux[i] = orderList[i];
-            return;
-        }
         mergeBySlope(source, orderList, aux, lo, mid, hi);
     }
 
     private void sortBySlope(Point source, Point[] orderList) {
-        Point[] aux = new Point[orderList.length];
-        for (int i = 0; i < orderList.length; i++) {
-            aux[i] = orderList[i];
-        }
+        Point[] aux = orderList.clone();
         sortBySlope(source, aux, orderList, 0, orderList.length - 1);
     }
+
 
     private Point[] resize(Point[] oldArray) {
         int n = oldArray.length;
@@ -131,7 +121,7 @@ public class FastCollinearPoints {
         return copy;
     }
 
-    private void mainFunc() {
+    private void mainFunc(Point[] pointList) {
         Point[] endPoint = new Point[pointList.length];
         double[] slope = new double[pointList.length];
 
@@ -146,40 +136,48 @@ public class FastCollinearPoints {
 
             sortBySlope(pointList[i], orderPoints);
 
-            for (int j = 0; j < orderPoints.length - 2; j++) {
+            for (int j = 0; j < orderPoints.length; j++) {
+                int depth = 0;
                 double baseSlope = pointList[i].slopeTo(orderPoints[j]);
                 for (int k = j + 1; k < orderPoints.length; k++) {
                     if (pointList[i].slopeTo(orderPoints[k]) != baseSlope) {
-                        if (k - j > 2 && canIn(endPoint, slope, orderPoints[k - 1], baseSlope)) {
+                        if (depth > 1) {
                             if (index == slope.length) {
                                 lineList = resize(lineList);
                                 endPoint = resize(endPoint);
                                 slope = resize(slope);
                             }
                             // 선분에 대한 정보(끝점, 기울기) 추가됨
-                            lineList[index] = new LineSegment(pointList[i], orderPoints[k - 1]);
-                            endPoint[index] = orderPoints[k - 1];
-                            slope[index] = baseSlope;
-                            index += 1;
+                            if (canIn(endPoint, slope, orderPoints[k - 1], baseSlope)) {
+                                lineList[index] = new LineSegment(pointList[i], orderPoints[k - 1]);
+                                endPoint[index] = orderPoints[k - 1];
+                                slope[index] = baseSlope;
+                                index += 1;
+                            }
                         }
                         j = k - 1;
                         break;
                     }
                     else if (pointList[i].slopeTo(orderPoints[k]) == baseSlope && k
                             == orderPoints.length - 1) {
-                        if (k - j > 1 && canIn(endPoint, slope, orderPoints[k], baseSlope)) {
+                        if (depth >= 1) {
                             if (index == slope.length) {
                                 lineList = resize(lineList);
                                 endPoint = resize(endPoint);
                                 slope = resize(slope);
                             }
-                            lineList[index] = new LineSegment(pointList[i], orderPoints[k]);
-                            endPoint[index] = orderPoints[k];
-                            slope[index] = baseSlope;
-                            index += 1;
+                            if (canIn(endPoint, slope, orderPoints[k], baseSlope)) {
+                                lineList[index] = new LineSegment(pointList[i], orderPoints[k]);
+                                endPoint[index] = orderPoints[k];
+                                slope[index] = baseSlope;
+                                index += 1;
+                            }
                         }
-                        j = k;
+                        j = k - 1;
                         break;
+                    }
+                    else {
+                        depth += 1;
                     }
                 }
             }
@@ -192,6 +190,5 @@ public class FastCollinearPoints {
         }
         return true;
     }
-
 
 }
