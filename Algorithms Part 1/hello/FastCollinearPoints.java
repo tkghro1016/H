2fceba565b6f2aa785ae2961f30ce36008cbfe3f
tkghro1016 <tkghro1016@gmail.com>
2021-06-15 21:30:21 +0900

    --
    --

--- Algorithms Part 1/hello/FastCollinearPoints.java
+++ Algorithms Part 1/hello/FastCollinearPoints.java
@@ -4,19 +4,17 @@
  *  Last modified:     May 23, 2021
  **************************************************************************** */
 
-
-import edu.princeton.cs.algs4.Insertion;
+import java.util.ArrayList;
 
 public class FastCollinearPoints {
-    private LineSegment[] lineList;
-    private int index;
+    private ArrayList<LineSegment> lineList;
+    private int lineIndex;
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
         }
-
 
         // input을 바로 가리키지 않게 복사
         Point[] pointList = new Point[points.length];
@@ -26,22 +24,23 @@ public class FastCollinearPoints {
             }
             pointList[i] = points[i];
         }
-        index = 0;
-        lineList = new LineSegment[pointList.length];
+        lineList = new ArrayList<>();
+        lineIndex = 0;
         sortByCoord(pointList);
         mainFunc(pointList);
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        return index;
+        return lineList.size();
     }
 
     // the line segments
     public LineSegment[] segments() {
-        LineSegment[] copy = new LineSegment[index];
-        for (int i = 0; i < index; i++) {
-            copy[i] = lineList[i];
+        // 직접 변수가 노출 되지 않도록 복사
+        LineSegment[] copy = new LineSegment[lineIndex];
+        for (int i = 0; i < lineIndex; i++) {
+            copy[i] = lineList.get(i);
         }
         return copy;
     }
@@ -57,10 +56,7 @@ public class FastCollinearPoints {
     }
 
     private void sortByCoord(Point[] pointList, Point[] aux, int lo, int hi) {
-        if (hi <= lo + 7) {
-            Insertion.sort(aux, lo, hi);
-            return;
-        }
+        if (hi <= lo) return;
         int mid = lo + (hi - lo) / 2;
         sortByCoord(aux, pointList, lo, mid);
         sortByCoord(aux, pointList, mid + 1, hi);
@@ -97,98 +93,54 @@ public class FastCollinearPoints {
         sortBySlope(source, aux, orderList, 0, orderList.length - 1);
     }
 
-
-    private Point[] resize(Point[] oldArray) {
-        int n = oldArray.length;
-        Point[] copy = new Point[2 * n];
-        for (int i = 0; i < n; i++)
-            copy[i] = oldArray[i];
-        return copy;
-    }
-
-    private LineSegment[] resize(LineSegment[] oldArray) {
-        int n = oldArray.length;
-        LineSegment[] copy = new LineSegment[2 * n];
-        for (int i = 0; i < n; i++)
-            copy[i] = oldArray[i];
-        return copy;
-    }
-
-    private double[] resize(double[] oldArray) {
-        int n = oldArray.length;
-        double[] copy = new double[2 * n];
-        for (int i = 0; i < n; i++) copy[i] = oldArray[i];
-        return copy;
-    }
-
     private void mainFunc(Point[] pointList) {
-        Point[] endPoint = new Point[pointList.length];
-        double[] slope = new double[pointList.length];
-
         for (int i = 0; i < pointList.length; i++) {
-            Point[] orderPoints = new Point[pointList.length - i - 1];
-            for (int j = i + 1; j < pointList.length; j++) {
-                if (pointList[i].compareTo(pointList[j]) == 0) {
-                    throw new IllegalArgumentException();
-                }
-                orderPoints[j - i - 1] = pointList[j];
-            }
-
-            sortBySlope(pointList[i], orderPoints);
-
-            for (int j = 0; j < orderPoints.length; j++) {
-                int depth = 0;
-                double baseSlope = pointList[i].slopeTo(orderPoints[j]);
-                for (int k = j + 1; k < orderPoints.length; k++) {
-                    if (pointList[i].slopeTo(orderPoints[k]) != baseSlope) {
-                        if (depth > 1) {
-                            if (index == slope.length) {
-                                lineList = resize(lineList);
-                                endPoint = resize(endPoint);
-                                slope = resize(slope);
-                            }
-                            // 선분에 대한 정보(끝점, 기울기) 추가됨
-                            if (canIn(endPoint, slope, orderPoints[k - 1], baseSlope)) {
-                                lineList[index] = new LineSegment(pointList[i], orderPoints[k - 1]);
-                                endPoint[index] = orderPoints[k - 1];
-                                slope[index] = baseSlope;
-                                index += 1;
-                            }
-                        }
-                        j = k - 1;
-                        break;
-                    }
-                    else if (pointList[i].slopeTo(orderPoints[k]) == baseSlope && k
-                            == orderPoints.length - 1) {
-                        if (depth >= 1) {
-                            if (index == slope.length) {
-                                lineList = resize(lineList);
-                                endPoint = resize(endPoint);
-                                slope = resize(slope);
-                            }
-                            if (canIn(endPoint, slope, orderPoints[k], baseSlope)) {
-                                lineList[index] = new LineSegment(pointList[i], orderPoints[k]);
-                                endPoint[index] = orderPoints[k];
-                                slope[index] = baseSlope;
-                                index += 1;
-                            }
-                        }
-                        j = k - 1;
-                        break;
+            Point[] slopeOrderList = pointList.clone();
+            sortBySlope(pointList[i], slopeOrderList);
+            double skipCriterionSlope = Double.NEGATIVE_INFINITY;
+            double countCriterionSlope = Double.NEGATIVE_INFINITY;
+            int count = 0;
+            for (int j = 1; j < slopeOrderList.length; j++) {
+                double currentSlope = pointList[i].slopeTo(slopeOrderList[j]);
+                boolean skip = (pointList[i].compareTo(slopeOrderList[j]) > 0) || (
+                        pointList[i].compareTo(slopeOrderList[j]) < 0
+                                && skipCriterionSlope == currentSlope);
+                if (!skip) {
+                    if (count == 0) {
+                        countCriterionSlope = currentSlope;
+                        count += 1;
                     }
                     else {
-                        depth += 1;
+                        if (countCriterionSlope != currentSlope && count > 2) {
+                            lineList.add(lineIndex++, new LineSegment(pointList[i],
+                                                                      slopeOrderList[j - 1]));
+                            countCriterionSlope = currentSlope;
+                            count = 1;
+                        }
+                        else if (countCriterionSlope == currentSlope
+                                && j == slopeOrderList.length - 1 && count >= 2) {
+                            lineList.add(lineIndex++, new LineSegment(pointList[i],
+                                                                      slopeOrderList[j]));
+                            countCriterionSlope = currentSlope;
+                        }
+                        else if (countCriterionSlope == currentSlope) {
+                            count += 1;
+                        }
+                        else {
+                            count = 0;
+                        }
                     }
+                }
+                else {
+                    if (count > 2) {
+                        lineList.add(lineIndex++, new LineSegment(pointList[i],
+                                                                  slopeOrderList[j - 1]));
+                    }
+                    count = 0;
+                    skipCriterionSlope = currentSlope;
+                    countCriterionSlope = Double.NEGATIVE_INFINITY;
                 }
             }
         }
     }
-
-    private boolean canIn(Point[] endpoints, double[] slopes, Point endPoint, double slope) {
-        for (int i = 0; i < endpoints.length; i++) {
-            if (slopes[i] == slope && endpoints[i] == endPoint) return false;
-        }
-        return true;
-    }
-
 }
