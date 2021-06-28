7953c56e730ed94427508d0c5104f14fc8099adf
tkghro1016 <tkghro1016@gmail.com>
2021-06-28 14:33:33 +0900

    --
    --

--- Algorithms Part 1/hello/Board.java
+++ Algorithms Part 1/hello/Board.java
@@ -12,6 +12,7 @@ public class Board {
     private final int[][] board;
     private final int size;
     private final int[] zeroIndex;
+    private final int manhattan;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
@@ -19,6 +20,9 @@ public class Board {
         size = tiles.length;
         board = new int[size][size];
         zeroIndex = new int[2];
+
+        int dist = 0;
+        int[] cord = new int[2];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 board[i][j] = tiles[i][j];
@@ -26,9 +30,48 @@ public class Board {
                     zeroIndex[0] = i;
                     zeroIndex[1] = j;
                 }
+                else {
+                    cord[0] = i;
+                    cord[1] = j;
+                    int[] properCord = where(board[i][j]);
+                    dist += abs(cord[0] - properCord[0]) + abs(cord[1] - properCord[1]);
+                }
             }
         }
+        manhattan = dist;
     }
+
+    // Neighbor-Board Constructor
+    // private Board(Board preBoard, int[] source, int[] target) {
+    //     int[] sourceCord = source.clone();
+    //     int[] targetCord = target.clone();
+    //     int nextManhattan = preBoard.manhattan;
+    //     size = preBoard.size;
+    //     int[] targetProperCord = where(preBoard.board[targetCord[0]][targetCord[1]]);
+    //     board = exchange(preBoard.board, sourceCord, targetCord);
+    //
+    //     if (preBoard.board[sourceCord[0]][sourceCord[1]] == 0) {
+    //         zeroIndex = targetCord;
+    //         nextManhattan -= abs(targetCord[0] - targetProperCord[0]);
+    //         nextManhattan -= abs(targetCord[1] - targetProperCord[1]);
+    //         nextManhattan += abs(sourceCord[0] - targetProperCord[0]);
+    //         nextManhattan += abs(sourceCord[1] - targetProperCord[1]);
+    //     }
+    //     else {
+    //         zeroIndex = preBoard.zeroIndex;
+    //         int[] sourceProperCord = where(preBoard.board[sourceCord[0]][sourceCord[1]]);
+    //         nextManhattan -= abs(sourceCord[0] - sourceProperCord[0]);
+    //         nextManhattan -= abs(sourceCord[1] - sourceProperCord[1]);
+    //         nextManhattan -= abs(targetCord[0] - targetProperCord[0]);
+    //         nextManhattan -= abs(targetCord[1] - targetProperCord[1]);
+    //         nextManhattan += abs(sourceCord[0] - targetProperCord[0]);
+    //         nextManhattan += abs(sourceCord[1] - targetProperCord[1]);
+    //         nextManhattan += abs(targetCord[0] - sourceProperCord[0]);
+    //         nextManhattan += abs(targetCord[1] - sourceProperCord[1]);
+    //     }
+    //     manhattan = nextManhattan;
+    // }
+
 
     // string representation of this board
     public String toString() {
@@ -62,30 +105,12 @@ public class Board {
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        int dist = 0;
-        int[] index = new int[2];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (board[i][j] == 0) continue;
-                index[0] = i;
-                index[1] = j + 1;
-                int[] key = where(board[i][j]);
-                dist += abs(index[0] - key[0]) + abs(index[1] - key[1]);
-            }
-        }
-        return dist;
+        return manhattan;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        if (board[size - 1][size - 1] != 0) return false;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == size - 1 && j == size - 1) break;
-                if (board[i][j] != i * size + j + 1) return false;
-            }
-        }
-        return true;
+        return manhattan == 0;
     }
 
     // does this board equal y?
@@ -95,6 +120,8 @@ public class Board {
         if (this == y) return true;
 
         if (this.size != ((Board) y).size) return false;
+        if (this.zeroIndex[0] != ((Board) y).zeroIndex[0]
+                || this.zeroIndex[1] != ((Board) y).zeroIndex[1]) return false;
         for (int i = 0; i < this.size; i++) {
             for (int j = 0; j < this.size; j++) {
                 if (this.board[i][j] != ((Board) y).board[i][j]) return false;
@@ -109,6 +136,9 @@ public class Board {
     //     return false;
     // }
 
+    // private Board getOuter() {
+    //     return this;
+    // }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
@@ -122,28 +152,55 @@ public class Board {
     }
 
     private class IteratorBoard implements Iterator<Board> {
-        private int[][] currBoard = new int[size - 1][size - 1];
-        private final int[][] preBoard = board;
-        private final int[] zeroRowCol = zeroIndex;
+        private Board[] boards = new Board[4];
+        private int count = 0;
+        private int current = 0;
+
+        public IteratorBoard() {
+            if (zeroIndex[0] != 0) {
+                int[] target = { zeroIndex[0] - 1, zeroIndex[1] };
+                boards[count++] = new Board(exchange(board, zeroIndex, target));
+            }
+            if (zeroIndex[0] != size - 1) {
+                int[] target = { zeroIndex[0] + 1, zeroIndex[1] };
+                boards[count++] = new Board(exchange(board, zeroIndex, target));
+            }
+            if (zeroIndex[1] != 0) {
+                int[] target = { zeroIndex[0], zeroIndex[1] - 1 };
+                boards[count++] = new Board(exchange(board, zeroIndex, target));
+            }
+            if (zeroIndex[1] != size - 1) {
+                int[] target = { zeroIndex[0], zeroIndex[1] + 1 };
+                boards[count++] = new Board(exchange(board, zeroIndex, target));
+            }
+        }
 
         public boolean hasNext() {
-            return false;
+            if (current >= count) return false;
+            return true;
         }
 
         public Board next() {
-            return null;
+            if (boards[current] == null) throw new java.util.NoSuchElementException();
+            return boards[current++];
         }
 
         public void remove() {
-
+            throw new UnsupportedOperationException();
         }
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        int[] source = new int[] { StdRandom.uniform(0, size), StdRandom.uniform(0, size) };
-        int[] target = new int[] { StdRandom.uniform(0, size), StdRandom.uniform(0, size) };
-        while (source[0] == target[0] && source[1] == target[1]) {
+        int[] source = { StdRandom.uniform(0, size), StdRandom.uniform(0, size) };
+        while (source[0] == zeroIndex[0] && source[1] == zeroIndex[1]) {
+            source[0] = StdRandom.uniform(0, size);
+            source[1] = StdRandom.uniform(0, size);
+        }
+
+        int[] target = { StdRandom.uniform(0, size), StdRandom.uniform(0, size) };
+        while (source[0] == target[0] && source[1] == target[1]
+                || target[0] == zeroIndex[0] && target[1] == zeroIndex[1]) {
             target[0] = StdRandom.uniform(0, size);
             target[1] = StdRandom.uniform(0, size);
         }
@@ -152,15 +209,15 @@ public class Board {
     }
 
     // return {rowIndex, colIndex};
-    // row: start with 0, end with size-1; col: start with 1, end with size;
+    // row: start with 0, end with size-1; col: start with 0, end with size - 1;
     private int[] where(int num) {
         int row = num / size;
-        int col = num % size;
-        if (col == 0) {
+        int col = num % size - 1;
+        if (col == -1) {
             row -= 1;
-            col = size;
+            col = size - 1;
         }
-        int[] index = new int[] { row, col };
+        int[] index = { row, col };
         return index;
     }
 
@@ -174,34 +231,66 @@ public class Board {
     }
 
     private int[][] exchange(int[][] arg, int[] source, int[] target) {
-        int tmp = arg[source[0]][source[1]];
-        arg[source[0]][source[1]] = arg[target[0]][target[1]];
-        arg[target[0]][target[1]] = tmp;
-        return arg;
+        int[][] copy = new int[arg.length][arg.length];
+        for (int i = 0; i < arg.length; i++) {
+            for (int j = 0; j < arg[i].length; j++) {
+                copy[i][j] = arg[i][j];
+            }
+        }
+        int tmp = copy[source[0]][source[1]];
+        copy[source[0]][source[1]] = copy[target[0]][target[1]];
+        copy[target[0]][target[1]] = tmp;
+        return copy;
     }
 
     // unit testing (not graded)
     public static void main(String[] args) {
-        int[][] tst = new int[][] { { 2, 3, 1 }, { 5, 6, 4 }, { 0, 8, 7 } };
-        int[][] tst1 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
-        int[][] tst2 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
-        Board bst = new Board(tst);
-        Board bst1 = new Board(tst1);
-        Board bst2 = new Board(tst2);
-        System.out.println(bst.toString());
-        System.out.println(bst1.toString());
-        System.out.println(bst.dimension());
-        System.out.println(bst.hamming());
-        System.out.println(bst1.hamming());
-        System.out.println(bst.isGoal());
-        System.out.println(bst1.isGoal());
-        System.out.println(bst.equals(bst1));
-        System.out.println(bst.twin().toString());
-        System.out.println(bst2.equals(bst1));
-        System.out.println(bst1.manhattan());
-        System.out.println(bst1.twin());
+        // int[][] tst = new int[][] { { 2, 3, 4 }, { 5, 6, 1 }, { 0, 8, 7 } };
+        // int[][] tst1 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+        // int[][] tst2 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+        // int[][] tst3 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 8, 0, 7 } };
+        // Board bst = new Board(tst);
+        // Board bst1 = new Board(tst1);
+        // Board bst2 = new Board(tst2);
+        // Board bst3 = new Board(tst3);
+        // System.out.println(bst);
+        // System.out.println(bst4);
+        // System.out.println(bst3);
+
+        // System.out.println(bst.dimension());
+        // System.out.println(bst.hamming());
+        // System.out.println(bst1.hamming());
+
+        // System.out.println(bst.isGoal());
+        // System.out.println(bst1.isGoal());
+
+        // System.out.println(bst.equals(bst1));
+        // System.out.println(bst2.equals(bst1));
+        // System.out.println(bst1.equals(bst1));
+
+        // System.out.println(bst1.twin());
+        // System.out.println(bst1);
+
+        int[][] tst4 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
+        Board bst4 = new Board(tst4);
+        System.out.println(bst4);
+        System.out.println(bst4.manhattan());
+        for (Board brd : bst4.neighbors()) {
+            System.out.println("=========");
+            System.out.println(brd);
+            System.out.println(brd.manhattan());
+        }
+        System.out.println(bst4);
+        System.out.println("***************");
+        Board bst4Twin = bst4.twin();
+        System.out.println(bst4Twin);
+        System.out.println(bst4Twin.manhattan());
+        for (Board brd : bst4Twin.neighbors()) {
+            System.out.println("=========");
+            System.out.println(brd);
+            System.out.println(brd.manhattan());
+        }
+
     }
 
 }
-
-
--- Algorithms Part 1/hello/Solver.java
+++ Algorithms Part 1/hello/Solver.java
@@ -4,10 +4,18 @@
  *  Last modified:     June 21, 2021
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Iterator;
+
 public class Solver {
+    private MinPQ<Integer> boardPQ;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        boardPQ.insert(initial.manhattan());
+
+
     }
 
     // is the initial board solvable? (see below)
@@ -22,7 +30,15 @@ public class Solver {
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return null;
+        return () -> new Iterator<Board>() {
+            public boolean hasNext() {
+                return false;
+            }
+
+            public Board next() {
+                return null;
+            }
+        };
     }
 
     // test client (see below)
